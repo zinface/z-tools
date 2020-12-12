35c95fb79a76bbf6d55b9cb002bb7934d5abd2c0
zinfacex <zinfacex@gmail.com>
2020-12-12 13:04:23 +0800

    update BuildSourceTgz.cmake

--- cmake/BuildSourceTgz.cmake
+++ cmake/BuildSourceTgz.cmake
@@ -5,34 +5,30 @@ string(TIMESTAMP BUILD_TIME "%Y%m%d")
 set(CPACK_GENERATOR "TGZ")
 set(CPACK_PACKAGE_FILE_NAME "${PROJECT_NAME}_${PROJECT_VERSION}-${BUILD_TIME}")
 
-# 子构建配置(组件管理)
-install(FILES Components/CMakeLists.txt
-    DESTINATION target/Components
+# 项目组件(Components)
+install(DIRECTORY Components
+    DESTINATION target
 )
-# 项目组件(DesktopGenerater)
-install(DIRECTORY Components/DesktopGenerater
-    DESTINATION target/Components
-)
-# 资源目录
+# 项目资源目录
 install(DIRECTORY resources
     DESTINATION target
 )
-# 可扩展构建模块
+
+# 项目 CMake 可扩展构建模块
 install(DIRECTORY cmake
     DESTINATION target
 )
-# 顶级构建配置
+# 项目 CMake 顶级构建配置
 install(FILES  CMakeLists.txt
     DESTINATION target
 )
 
-# 项目组织代码
-install(FILES
-    main.cpp
+# 项目组织代码、目录
+install(FILES main.cpp
     DESTINATION target
 )
-install(DIRECTORY
-    src
+
+install(DIRECTORY src
     DESTINATION target
 )
 

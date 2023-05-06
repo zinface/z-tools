51c4f4ec186ca3d31ac0d357347651bbc636e995
zinface <zinface@163.com>
2023-05-06 10:50:00 +0800

    cmake: 调整 SparkDesktopMacros 的部分定义

--- cmake/SparkDesktopMacros.cmake
+++ cmake/SparkDesktopMacros.cmake
@@ -1,17 +1,19 @@
+# SparkDesktopMacros.cmake
 
-macro(spark_desktop_macros _APP_NAME _APP_NAME_ZH_CN _APP_COMMENT _APP_TYPE _APP_EXECUTE_PATH _APP_EXECUTE_ICON_PATH _APP_CATEGORIES)
-    set(APP_NAME ${_APP_NAME})
-    set(APP_NAME_ZH_CN ${_APP_NAME_ZH_CN})
-    set(APP_COMMENT ${_APP_COMMENT})
-    set(APP_TYPE ${_APP_TYPE})
-    set(APP_EXECUTE_PATH ${_APP_EXECUTE_PATH})
-    set(APP_EXECUTE_ICON_PATH ${_APP_EXECUTE_ICON_PATH})
-    set(APP_CATEGORIES ${_APP_CATEGORIES})
+macro(spark_desktop_macros)
+    set(APP_NAME              ${ARGV0})
+    set(APP_NAME_ZH_CN        ${ARGV1})
+    set(APP_COMMENT           ${ARGV2})
+    set(APP_TYPE              ${ARGV3})
+    set(APP_EXECUTE_PATH      ${ARGV4})
+    set(APP_EXECUTE_ICON_PATH ${ARGV5})
+    set(APP_CATEGORIES        ${ARGV6})
+    set(APP_MIME_TYPE         ${ARGV7})
     configure_file(cmake/spark-desktop.desktop.in
-        ${CMAKE_BINARY_DIR}/${_APP_NAME}.desktop
+        ${CMAKE_BINARY_DIR}/${ARGV0}.desktop
     )
-    set(DESKTOP_FILE ${CMAKE_BINARY_DIR}/${_APP_NAME}.desktop)
-endmacro(spark_desktop_macros _APP_NAME _APP_NAME_ZH_CN _APP_COMMENT _APP_TYPE _APP_EXECUTE_PATH _APP_EXECUTE_ICON_PATH _APP_CATEGORIES)
+    set(DESKTOP_FILE ${CMAKE_BINARY_DIR}/${ARGV0}.desktop)
+endmacro(spark_desktop_macros)
 
 # include(cmake/SparkDesktopMacros.cmake)
 # 内容默认应用名称: Name= 应与项目名称相同
@@ -23,6 +25,7 @@ endmacro(spark_desktop_macros _APP_NAME _APP_NAME_ZH_CN _APP_COMMENT _APP_TYPE _
     # 执行程序: Exec=
     # 图标路径: Icon=
     # 应用分类: Category=
+    # MIME类型: MimeType=
 # )
 
 # configure_file(<input> <output>

24f608b192a26da2244d6099e283320b59f8493a
zinface <zinface@163.com>
2023-12-06 17:55:33 +0800

    repo: 处理双屏下应用出现的位置

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,22 @@
 #include <QDebug>
 #include <QDesktopWidget>
 #include <QMainWindow>
+#include <QScreen>
 #include <aptmanager.h>
-
 #include "config.h"
+
+void moveCenter(QWidget &wm) {
+    QScreen *screen = QApplication::screenAt(QCursor::pos());
+
+    auto desktop = screen->geometry();
+    
+    // 窗口的 x 位置
+    auto afterX = (desktop.width() - wm.width())/ 2;
+    // 窗口的 y 位置，居中靠上
+    auto afterY = (desktop.height() - wm.height()) / 2;
+    
+    wm.move(afterX + desktop.x(), afterY + desktop.y());
+}
 
 int main(int argc, char *argv[])
 {
@@ -15,7 +28,8 @@ int main(int argc, char *argv[])
     a.setApplicationName("软件包管理器 - v" PROJECT_VERSION);
     AptManager wm;
     wm.show();
-    QDesktopWidget *desktop = QApplication::desktop();
-    wm.move((desktop->width() - wm.width()) /2,(desktop->height() - wm.height()) /2);
+    
+    moveCenter(wm);
+    
     return a.exec();
 }

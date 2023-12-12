cd6420040b7b3174fe4cec39255e147f2f25a196
zinface <zinface@163.com>
2023-12-12 14:14:22 +0800

    repo: 处理双屏下应用出现的位置

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,27 @@
 #include <QDesktopWidget>
 #include <QMainWindow>
 
+#include <QScreen>
+
+void moveCenter(QWidget &wm) {
+    QScreen *screen = QApplication::screenAt(QCursor::pos());
+    
+    auto desktop = screen->geometry();
+    
+    auto afterX = (desktop.width() - wm.width())/ 2;
+    auto afterY = (desktop.height() - wm.height()) /2;
+    
+    wm.move(afterX + desktop.x(), afterY + desktop.y());
+}
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     a.setApplicationName("桌面图标生成器 - v1.2.1");
     MainWindow wm;
     wm.show();
-    QDesktopWidget *desktop = QApplication::desktop();
-    wm.move((desktop->width() - wm.width()) /2,(desktop->height() - wm.height()) /2);
+    
+    moveCenter(wm);
+
     return a.exec();
 }

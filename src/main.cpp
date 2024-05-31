2d38b6f1ccbe2db4fb31e5e3673979b9a6cab29d
zinface <zinface@163.com>
2024-05-31 14:23:05 +0800

    repo: 版本提升至 v0.3

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    a.setApplicationName("图片浏览器 - v0.2");
+    a.setApplicationName("图片浏览器 - v0.3");
 
     MainWindow wm;
     wm.show();

940bcdf5c5ff306d95e57adff45d29c5693ad494
zinface <zinface@163.com>
2024-06-03 19:28:29 +0800

    repo: 版本提升至 v0.4.3

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    a.setApplicationName("图片浏览器 - v0.4.2");
+    a.setApplicationName("图片浏览器 - v0.4.3");
 
     QCommandLineParser parser;
     parser.setApplicationDescription("图片浏览器");

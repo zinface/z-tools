d122e3a1420ac5945e59028aef5acc55c3eeb36e
zinface <zinface@163.com>
2024-12-24 00:49:14 +0800

    build: 更新 SparkWithGit.cmake 并初始化模块
    - 在 CMakeLists.txt 中添加 spark_initialize_modules() 函数调用
    - 更新 SparkWithGit.cmake 文件结构，增加 spark_initialize_modules 宏
    - 添加多个 Spark 构建配置文件，包括 Qt5、宏扩展、框架配置等
    - 优化 spark_include 宏，使用绝对路径进行文件存在性检查
    - 调整日志输出，增加 "[SparkInclude]" 前缀

--- cmake/SparkWithGit.cmake
+++ cmake/SparkWithGit.cmake
@@ -44,16 +44,16 @@ endfunction(spark_framework_from_git)
 macro(spark_include)
     set(_spark_files ${ARGN})
     foreach(_spark_file IN LISTS _spark_files)
-        if(EXISTS "${_spark_file}")
+        if(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/${_spark_file})
             include(${_spark_file})
             continue()
-        endif(EXISTS "${_spark_file}")
+        endif(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/${_spark_file})
         file(GLOB_RECURSE _file RELATIVE "${SPARK_FRAMEWORK_TO}" ${_spark_file})
         list(FILTER _file EXCLUDE REGEX "\\.\\.")
         
         if(NOT "${_file}" STREQUAL "")
             if(EXISTS "${SPARK_FRAMEWORK_TO}/${_file}")
-                message("FOUND: ${SPARK_FRAMEWORK_TO}/${_file}")
+                message("[SparkInclude] ${SPARK_FRAMEWORK_TO}/${_file}")
                 include("${SPARK_FRAMEWORK_TO}/${_file}")
             endif(EXISTS "${SPARK_FRAMEWORK_TO}/${_file}")
         else()
@@ -76,6 +76,7 @@ spark_framework_from_git(
         spark.find-dtk
         spark.find-library
         spark.macros-extend
+        spark.qt-qml-macros
         spark.build-graphviz
 
         spark.framework
@@ -87,29 +88,54 @@ spark_framework_from_git(
         spark.desktop-macro
         spark.deb-package
         spark.appimage-macros-online
+        spark.parasitic-build
 )
 
+# spark_initialize_modules 
+    # 用于初始化导入标准扩展组件宏
+macro(spark_initialize_modules)
+
+    # 用于 spark_include 引入相关的列表项，每一项都是可引用的构建模块
+    spark_include(
+        SparkEnvConfig.cmake
+        SparkMacrosConfig.cmake
+        SparkFindQt5Config.cmake
+        # SparkFindQt6Config.cmake
+        # SparkFindDtkConfig.cmake
+        # SparkFindLibraries.cmake
+        # SparkQtQmlMacros.cmake
+        SparkMacrosExtendConfig.cmake
+
+        SparkFramework.cmake
+        # SparkCMakeInfoAfterConfig.cmake
+        # SparkExternalProject.cmake
+
+        # SparkTranslatorConfig.cmake
+        SparkInstallMacrosConfig.cmake
+        SparkBuildGraphviz.cmake
+        SparkDesktopMacros.cmake
+        SparkDebPackageConfig.cmake
+        # SparkAppimageConfig.cmake
+        # SparkParasiticBuild.cmake
+    )
+
+endmacro(spark_initialize_modules)
+
+
+# 按需开启(直接解除注释)
+# spark_include(SparkCMakeInfoAfterConfig.cmake)
+
+
+# 使用方式：
+# 1. 如果要解除以上注释，则在 project 之前 include
+# 2. initialize 应在 project 指令后使用，如果要解除 SparkCMakeInfoAfter 的话
+# 3. 不使用 Before 与 After 信息时，以下可在 project 指令前后任意位置使用
+ 
 # include(cmake/SparkWithGit.cmake)
+# 
+# spark_initialize_modules()
 
-# 用于 spark_include 引入相关的列表项，每一项都是可引用的构建模块
-spark_include(
-    SparkEnvConfig.cmake
-    SparkMacrosConfig.cmake
-    SparkFindQt5Config.cmake
-#     SparkFindQt6Config.cmake
-    # SparkFindDtkConfig.cmake
-#     SparkFindLibraries.cmake
-    SparkMacrosExtendConfig.cmake
 
-    SparkFramework.cmake
-#     SparkCMakeInfoAfterConfig.cmake
-#     SparkCMakeInfoBeforeConfig.cmake
-#     SparkExternalProject.cmake
 
-#     SparkTranslatorConfig.cmake
-    SparkInstallMacrosConfig.cmake
-    SparkBuildGraphviz.cmake
-    SparkDesktopMacros.cmake
-    SparkDebPackageConfig.cmake
-#     SparkAppimageConfig.cmake
-)
+# 更新模块：
+# wget -O cmake/SparkWithGit.cmake https://gitee.com/zinface/spark.framework/raw/master/cmake/SparkWithGit.cmake

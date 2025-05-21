ea1dec713684d76d1991f68e1208acab5816ce77
zinface <zinface@163.com>
2025-05-21 09:44:14 +0800

    refactor(z-tools-desktop-generater): 重构代码并引入新功能
    - 引入 spark-framework-features 框架，包含 version 和 screenutil 模块
    - 重写 moveCenter 函数，使用 ScreenUtil 类替代手动计算屏幕位置
    - 更新应用名称和版本显示方式，使用 PROJECT_VERSION 宏
    - 删除冗余代码，提高代码可读性和维护性

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,24 @@
 #include "mainwindow.h"
 
 #include <QApplication>
-#include <QDebug>
-#include <QDesktopWidget>
 #include <QMainWindow>
 #include <bamfdialog.h>
 
-#include <QScreen>
-
-void moveCenter(QWidget &wm) {
-    QScreen *screen = QApplication::screenAt(QCursor::pos());
-    
-    auto desktop = screen->geometry();
-    
-    auto afterX = (desktop.width() - wm.width())/ 2;
-    auto afterY = (desktop.height() - wm.height()) /2;
-    
-    wm.move(afterX + desktop.x(), afterY + desktop.y());
-}
+#include <screenutil.h>
+#include <version.h>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    a.setApplicationName("桌面图标生成器 - v1.2.1");
+    a.setApplicationName("z-tools-desktop-generater");
+    a.setApplicationDisplayName("桌面图标生成器 - v" PROJECT_VERSION);
 
     qRegisterMetaType<Application>("Application");
 
     MainWindow wm;
     wm.show();
     
-    moveCenter(wm);
+    ScreenUtil::moveCenterForCursor(wm);
 
     return a.exec();
 }

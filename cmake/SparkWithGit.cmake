e924c87d648d9b851fa74781f4b6e5ff57ba49d4
zinface <zinface@163.com>
2026-03-09 19:07:58 +0800

    build: 改进 cmake-spark 引入方式

--- cmake/SparkWithGit.cmake
+++ cmake/SparkWithGit.cmake
@@ -41,19 +41,26 @@ function(spark_framework_from_git)
 
 endfunction(spark_framework_from_git)
 
-macro(spark_include )
+macro(spark_include)
     set(_spark_files ${ARGN})
     foreach(_spark_file IN LISTS _spark_files)
+        if(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/${_spark_file})
+            include(${_spark_file})
+            continue()
+        endif(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/${_spark_file})
         file(GLOB_RECURSE _file RELATIVE "${SPARK_FRAMEWORK_TO}" ${_spark_file})
         list(FILTER _file EXCLUDE REGEX "\\.\\.")
-        if(EXISTS "${SPARK_FRAMEWORK_TO}/${_file}")
-            message("FOUND: ${SPARK_FRAMEWORK_TO}/${_file}")
-            include("${SPARK_FRAMEWORK_TO}/${_file}")
+        
+        if(NOT "${_file}" STREQUAL "")
+            if(EXISTS "${SPARK_FRAMEWORK_TO}/${_file}")
+                message("[SparkInclude] ${SPARK_FRAMEWORK_TO}/${_file}")
+                include("${SPARK_FRAMEWORK_TO}/${_file}")
+            endif(EXISTS "${SPARK_FRAMEWORK_TO}/${_file}")
         else()
-            message(WARNING "NOT FOUND: ${SPARK_FRAMEWORK_TO}/${_file}")
-        endif(EXISTS "${SPARK_FRAMEWORK_TO}/${_file}")
+            message(WARNING "NOT FOUND: ${_spark_file}")
+        endif(NOT "${_file}" STREQUAL "")
     endforeach(_spark_file IN LISTS _spark_files)
-endmacro(spark_include _spark_file)
+endmacro(spark_include)
 
 
 # 引入的所有 Spark 构建模块
@@ -69,6 +76,7 @@ spark_framework_from_git(
         spark.find-dtk
         spark.find-library
         spark.macros-extend
+        spark.qt-qml-macros
         spark.build-graphviz
 
         spark.framework
@@ -80,29 +88,54 @@ spark_framework_from_git(
         spark.desktop-macro
         spark.deb-package
         spark.appimage-macros-online
+        spark.parasitic-build
 )
 
+# spark_initialize_modules 
+    # 用于初始化导入标准扩展组件宏
+macro(spark_initialize_modules)
+
+    # 用于 spark_include 引入相关的列表项，每一项都是可引用的构建模块
+    spark_include(
+        SparkEnvConfig.cmake
+        SparkMacrosConfig.cmake
+        SparkFindQt5Config.cmake
+        # SparkFindQt6Config.cmake
+        # SparkFindDtkConfig.cmake
+        SparkFindLibraries.cmake
+        # SparkQtQmlMacros.cmake
+        SparkMacrosExtendConfig.cmake
+
+        SparkFramework.cmake
+        # SparkCMakeInfoAfterConfig.cmake
+        # SparkExternalProject.cmake
+
+        # SparkTranslatorConfig.cmake
+        SparkInstallMacrosConfig.cmake
+        # SparkBuildGraphviz.cmake
+        # SparkDesktopMacros.cmake
+        # SparkDebPackageConfig.cmake
+        # SparkAppimageConfig.cmake
+        # SparkParasiticBuild.cmake
+    )
+
+endmacro(spark_initialize_modules)
+
+
+# 按需开启(直接解除注释)
+# spark_include(SparkCMakeInfoAfterConfig.cmake)
+
+
+# 使用方式：
+# 1. 如果要解除以上注释，则在 project 之前 include
+# 2. initialize 应在 project 指令后使用，如果要解除 SparkCMakeInfoAfter 的话
+# 3. 不使用 Before 与 After 信息时，以下可在 project 指令前后任意位置使用
+ 
 # include(cmake/SparkWithGit.cmake)
+# 
+# spark_initialize_modules()
 
-# 用于 spark_include 引入相关的列表项，每一项都是可引用的构建模块
-# spark_include(
-#     SparkEnvConfig.cmake
-#     SparkMacrosConfig.cmake
-#     SparkFindQt5Config.cmake
-#     SparkFindQt6Config.cmake
-#     SparkFindDtkConfig.cmake
-#     SparkFindLibraries.cmake
-#     SparkMacrosExtendConfig.cmake
 
-#     SparkFramework.cmake
-#     SparkCMakeInfoAfterConfig.cmake
-#     SparkCMakeInfoBeforeConfig.cmake
-#     SparkExternalProject.cmake
 
-#     SparkTranslatorConfig.cmake
-#     SparkInstallMacrosConfig.cmake
-#     SparkBuildGraphviz.cmake
-#     SparkDesktopMacros.cmake
-#     SparkDebPackageConfig.cmake
-#     SparkAppimageConfig.cmake
-# )
+# 更新模块：
+# wget -O cmake/SparkWithGit.cmake https://gitee.com/zinface/spark.framework/raw/master/cmake/SparkWithGit.cmake
